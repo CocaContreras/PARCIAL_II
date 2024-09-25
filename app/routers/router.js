8b193cf2b85d841105cf19c6308771d13ff37aad
CocaContreras <oc26686@gmail.com>
2024-09-24 22:58:31 -0600

    first commit

---
+++ app/routers/router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const Usuario = require('../controllers/usuario.controller.js');
+
+// Rutas para el controlador de Usuario
+
+router.post('/api/usuarios/create', Usuario.create);
+router.get('/api/usuarios/all', Usuario.findAll);
+router.get('/api/usuarios/onebyid/:id', Usuario.findById);
+router.put('/api/usuarios/update/:id', Usuario.update);
+router.delete('/api/usuarios/delete/:id', Usuario.delete);
+
+module.exports = router;
