3bcdb2d130513cb14f20ea30a8f25ca172c0a091
CocaContreras <oc26686@gmail.com>
2024-09-25 18:56:51 -0600

    addTarea

--- app/routers/router.js
+++ app/routers/router.js
@@ -21,4 +21,14 @@ router.get('/api/proyectos/onebyid/:id', Proyecto.findById);
 router.put('/api/proyectos/update/:id', Proyecto.update);
 router.delete('/api/proyectos/delete/:id', Proyecto.delete);
 
+const Tarea = require('../controllers/tarea.controller.js');
+
+// Rutas para el controlador de Tareas
+
+router.post('/api/tareas/create', Tarea.create);
+router.get('/api/tareas/all', Tarea.findAll);
+router.get('/api/tareas/onebyid/:id', Tarea.findById);
+router.put('/api/tareas/update/:id', Tarea.update);
+router.delete('/api/tareas/delete/:id', Tarea.delete);
+
 module.exports = router;
